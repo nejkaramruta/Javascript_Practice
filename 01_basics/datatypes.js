367a573bdd4c971c358b7b43ba75b554403595a2
nejkaramruta <134083136+nejkaramruta@users.noreply.github.com>
2023-08-29 05:48:38 +0000

    variable and datatype

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+
+// two types of datatypes => 1) primitive datatypes 2) non-primitive datatypes
+
+//Number => range 2 to power 53
+//string => "", ''
+//bingint => 
+//boolean => true/false
+//null => standalone value
+// undefined 
+//symbol => unique
+
+
+// object , array 
+
+let name="amruta"
+console.log(typeof name);
+
+let isLogged=false;
+console.log(isLogged)
+ 
---
+++ 01_basics/variable.js
@@ -0,0 +1,22 @@
+
+// let , const , var 
+
+// let => can not redeclare but reasign
+
+//const => constant value
+
+// var => redclare and reasign 
+
+let a=20;
+a=50;
+
+console.log(a)
+
+const b=50;
+console.log(b)
+
+var state ="maharashtra"
+
+var state= " gujrat"
+
+console.log(state)
